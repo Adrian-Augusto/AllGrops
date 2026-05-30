78dbf12a453d4191dcdadef7096905a16d178b91
Adrian <adriansilva7272@gmail.com>
2026-05-30 11:02:09 -0300

    chore: add Swagger UI (swagger-ui-express) at /api/docs

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
+
+  // Serve Swagger UI at /api/docs
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(3000);
   console.log('Application is running on: http://localhost:3000/api');
+  console.log('Swagger docs available at: http://localhost:3000/api/docs');
 }
 
 bootstrap();
